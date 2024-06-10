5793344420fba1e3743ee4504700c99a8994e765
cassiejc <133578041+cassiejc@users.noreply.github.com>
2024-06-10 11:40:57 +0700

    Create boot1.py

---
+++ boot1.py
@@ -0,0 +1,34 @@
+import time
+import umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+ssid = 'IoT_Dev'
+password = 'elektro234'
+
+station = network.WLAN(network.STA_IF)
+station.active(True)
+station.connect(ssid,password)
+
+while station.isconnected() == False:
+  pass
+  
+print("CONNECTED")
+print(station.ifconfig)
+
+
+
+
+mqtt_server = '192.168.41.106'
+
+client_id = ubinascii.hexlify(machine.unique_id())
+
+
